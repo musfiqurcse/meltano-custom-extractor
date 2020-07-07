e3a6be786f187cb08da73db59541c9fb3fce19f8
musfiqurcse <md.musfiqurcse@outlook.com>
2020-07-07 08:22:02 +0200

    [Update] Meltano table and model modified

--- transform/models/tap_covid_19/transform/risk_area.sql
+++ transform/models/tap_covid_19/transform/risk_area.sql
@@ -1,18 +1,18 @@
 with source as (
-    select * from {{"tap_covid_19.eu_ecdc_daily"}}
+    select * from {{var("schema")}}.eu_ecdc_daily
 ),
 risk_area as (
     select
         country,
         SUM(deaths),
         CASE WHEN
-          SUM(deaths) :: DECIMAL < 1000
-          THEN '1 - Small Risk (<1k)'
-        WHEN SUM(deaths) :: DECIMAL >= 1000 AND SUM(deaths) :: DECIMAL < 2500
-          THEN '2 - Medium Risk (1k - 2.5k)'
-        WHEN SUM(deaths) :: DECIMAL >= 2500 AND SUM(deaths) :: DECIMAL < 5000
-          THEN '3 - Moderate Risk (2.5k - 5k)'
-        WHEN SUM(deaths) :: DECIMAL >= 5000
+          SUM(deaths) :: DECIMAL < 100
+          THEN '1 - Small Risk '
+        WHEN SUM(deaths) :: DECIMAL >= 100 AND SUM(deaths) :: DECIMAL < 1000
+          THEN '2 - Medium Risk'
+        WHEN SUM(deaths) :: DECIMAL >= 1000 AND SUM(deaths) :: DECIMAL < 2000
+          THEN '3 - Moderate Risk'
+        WHEN SUM(deaths) :: DECIMAL >= 2000
           THEN '4 - High Risk (>5k)'
         ELSE '5 - Unknown'
         END  AS risk_level
